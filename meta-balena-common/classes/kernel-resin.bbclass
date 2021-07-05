hwkyai/meta-balena

Add support for rootfs on MD RAID1
This commit does two things:
- Build in necessary kernel modules
- Add the mdadm userspace utility to rootfs and initrd

Change-type: minor
Signed-off-by: Michal Toman <michalt@balena.io>

meta-balena-common/classes/kernel-resin.bbclass
@@ -101,6 +101,7 @@ RESIN_CONFIGS ?= " \
     uprobes \
     task-accounting \
     ipv6_mroute \
+    mdraid \
     "
 
 #
@@ -563,6 +564,14 @@ RESIN_CONFIGS[ad5446] = " \
 # enable user space probes support
 RESIN_CONFIGS[uprobes] = " \
     CONFIG_UPROBE_EVENTS=y \
+"
+
+# enable rootfs on RAID1
+RESIN_CONFIGS[mdraid] = " \
+    CONFIG_MD=y \
+    CONFIG_BLK_DEV_MD=y \
+    CONFIG_MD_RAID1=y \
+    CONFIG_MD_AUTODETECT=y \
 "
 
 ###########

resin-os/meta-resin

meta-balena-common/classes/kernel-resin.bbclass
@@ -102,6 +102,7 @@ BALENA_CONFIGS ?= " \
     task-accounting \
     ipv6_mroute \
     disable_hung_panic \
+    mdraid \
     "
 
 #
@@ -567,6 +568,14 @@ BALENA_CONFIGS[uprobes] = " \
 BALENA_CONFIGS[disable_hung_panic] = " \
     CONFIG_BOOTPARAM_HUNG_TASK_PANIC=n \
     "
+
+# enable rootfs on RAID1
+BALENA_CONFIGS[mdraid] = " \
+    CONFIG_MD=y \
+    CONFIG_BLK_DEV_MD=y \
+    CONFIG_MD_RAID1=y \
+    CONFIG_MD_AUTODETECT=y \
+"
 
 ###########
 # HELPERS #